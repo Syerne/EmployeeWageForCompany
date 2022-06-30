85058ae33fd1e3e58cdec899c1c590732aa18e14
Syerne <shubhamyerne1994@gmail.com>
2022-06-30 15:51:20 +0530

    Attendence Check

--- EmployeeWageForCompany.java
+++ EmployeeWageForCompany.java
@@ -1,10 +1,26 @@
 package com.employee.wage;
 
 public class EmployeeWageForCompany {
+    public static final int EMP_IS_PRESENT = 1;
+
     public static void main(String[] args) {
-        welcomeMessage();
+        EmployeeWageForCompany empwage = new EmployeeWageForCompany();
+        empwage.welcomeMessage();
+        empwage.attendanceCheck();
+
     }
-    public static void welcomeMessage(){
+
+    public void welcomeMessage() {
         System.out.println("welcome to the Employee Wage Computation for Company");
+    }
+
+    public void attendanceCheck() {
+
+        int empRandomCheck = (int) (Math.random() * 2);
+        if (empRandomCheck == EMP_IS_PRESENT) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
