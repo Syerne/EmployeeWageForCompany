8147a1ecd66b1f9ef65a10dfb12874014664fd57
Syerne <shubhamyerne1994@gmail.com>
2022-06-30 15:39:26 +0530

    Added : welcome Mesggae

---
+++ EmployeeWageForCompany.java
@@ -0,0 +1,10 @@
+package com.employee.wage;
+
+public class EmployeeWageForCompany {
+    public static void main(String[] args) {
+        welcomeMessage();
+    }
+    public static void welcomeMessage(){
+        System.out.println("welcome to the Employee Wage Computation for Company");
+    }
+}
